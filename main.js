2d52ad11f29a7015b5aa18ca85b77427fe86f4b4
danainjax <danainjax@gmail.com>
2022-03-08 16:06:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//compiling and variables
+var danainjax = "Hi, I am dana";
+console.log(danainjax);
+var danainjax2 = function () { };
---
+++ main.ts
@@ -0,0 +1,6 @@
+//compiling and variables
+
+const danainjax = "Hi, I am dana";
+console.log(danainjax);
+
+const danainjax2 = () => {};
